cd2bd8cf1ab33ef456e537897a0d6616f00e9fa5
nicht762 <nicht762@gmail.com>
2023-04-20 15:56:36 +0300

    should work

--- src/gnss_reader/gnss_reader/gnss_reader.py
+++ src/gnss_reader/gnss_reader/gnss_reader.py
@@ -1,20 +1,47 @@
-import pymap3
+import rclpy
+from rclpy.node import Node
+from geometry_msgs.msg import PoseStamped, Pose
+import pymap3d
 from serial import Serial
 from pynmeagps import NMEAReader, NMEAMessage
 
-ellipsoid = pymap3d.Ellipsoid()
-# base
-lat_0 = 55.760057829964516
-lon_0 = 48.75896086562219
-hgt_0 = 199.37
 
-stream = Serial('/dev/ttyACM0', 115200, timeout=3)
-nmr = NMEAReader(stream)
+class GnssPub(Node):
+    def __init__(self):
+        super().__init__('gnss_reader')
+        self.publisher_ = self.create_publisher(Pose, 'gnss_pose', 10)
+        timer_period = 0.5
+        self.create_timer(timer_period, self.timer_cb)
 
-for (raw_data, parsed_data) in nmr:
-    if parsed_data.msgID == 'GGA':
-        #print(parsed_data)
-        east, north, up = pymap3d.geodetic2enu(parsed_data.lat, parsed_data.lon, parsed_data.alt,
-                                               lat_0, lon_0, hgt_0,
-                                               ell=ellipsoid, deg=True)
-        print(east, north, up)
+        self.ellipsoid = pymap3d.Ellipsoid()
+        # base
+        self.lat_0 = 55.760057829964516
+        self.lon_0 = 48.75896086562219
+        self.hgt_0 = 199.37
+
+        self.stream = Serial('/dev/ttyACM0', 115200, timeout=3)
+        self.nmr = NMEAReader(self.stream)
+
+    def timer_cb(self):
+        for (raw_data, parsed_data) in self.nmr:
+            if parsed_data.msgID == 'GGA':
+                # print(parsed_data)
+                east, north, up = pymap3d.geodetic2enu(parsed_data.lat, parsed_data.lon, parsed_data.alt,
+                                                       self.lat_0, self.lon_0, self.hgt_0,
+                                                       ell=self.ellipsoid, deg=True)
+                print(east, north, up)
+                msg = Pose()
+                # ENU
+                msg.position.x = east
+                msg.position.y = north
+                msg.position.z = up
+                self.publisher_.publish(msg)
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    gnss_pub = GnssPub()
+    rclpy.spin(gnss_pub)
+
+if __name__ =='__main__':
+    main()
